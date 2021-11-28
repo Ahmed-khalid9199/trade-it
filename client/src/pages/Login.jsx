fa38c3b1c6732f9be4049ae11e018479f285a4f3
ahmed-khalid9199 <ahmedkhalid9199@gmail.com>
2021-11-28 19:21:33 +0500

    login register styling

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,6 +8,8 @@ import { userActions } from "../store/user";
 import axios from "axios";
 import bcrypt from "bcryptjs";
 
+import background from "../assets/images/background1.jpg";
+
 const Login = () => {
   const [noUser, setNoUser] = useState(false);
   const [username, setUsername] = useState("");
@@ -63,62 +65,68 @@ const Login = () => {
     }
   };
   return (
-    <Row className="justify-content-center align-items-center vh-100 vw-100">
-      <Card style={{ width: "25rem" }}>
-        <Card.Body>
-          <Card.Title as="h2" className="text-center">
-            Login
-          </Card.Title>
-          <hr />
-          <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicUser">
-              <Form.Label>Email/ Username</Form.Label>
-              <Form.Control
-                value={username}
-                onChange={(e) => {
-                  setUsername(e.target.value);
-                }}
-                className={!usernameIsValid && !username ? "is-invalid" : ""}
-                type="text"
-                placeholder="Enter email/username"
-              />
-              {noUser && (
-                <Form.Text style={{ color: "red" }}>
-                  No such user exists!
-                </Form.Text>
-              )}
-            </Form.Group>
+    <>
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="background"
+      ></div>
+      <Row className="justify-content-center align-items-center vh-100 vw-100">
+        <Card className="form" style={{ width: "25rem" }}>
+          <Card.Body>
+            <Card.Title as="h2" className="text-center">
+              Login
+            </Card.Title>
+            <hr />
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="formBasicUser">
+                <Form.Label>Email/ Username</Form.Label>
+                <Form.Control
+                  value={username}
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
+                  className={!usernameIsValid && !username ? "is-invalid" : ""}
+                  type="text"
+                  placeholder="Enter email/username"
+                />
+                {noUser && (
+                  <Form.Text style={{ color: "red" }}>
+                    No such user exists!
+                  </Form.Text>
+                )}
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-                className={!passIsValid && !password ? "is-invalid" : ""}
-                type="password"
-                placeholder="Password"
-              />
-              {!passIsCorrect && (
-                <Form.Text style={{ color: "red" }}>
-                  Password is wrong!
-                </Form.Text>
-              )}
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  className={!passIsValid && !password ? "is-invalid" : ""}
+                  type="password"
+                  placeholder="Password"
+                />
+                {!passIsCorrect && (
+                  <Form.Text style={{ color: "red" }}>
+                    Password is wrong!
+                  </Form.Text>
+                )}
+              </Form.Group>
 
-            <div className="d-grid gap-3">
-              <Button size="lg" variant="primary" type="submit">
-                Login
-              </Button>
-              <Card.Subtitle className="mb-2 text-muted">
-                New User? <Card.Link href="/register">register</Card.Link>
-              </Card.Subtitle>
-            </div>
-          </Form>
-        </Card.Body>
-      </Card>
-    </Row>
+              <div className="d-grid gap-3">
+                <Button size="lg" variant="primary" type="submit">
+                  Login
+                </Button>
+                <Card.Subtitle className="mb-2 text-muted">
+                  New User? <Card.Link href="/register">register</Card.Link>
+                </Card.Subtitle>
+              </div>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Row>
+    </>
   );
 };
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import { Row, Form, Button, Card } from "react-bootstrap";
 import axios from "axios";
 import bcrypt from "bcryptjs";
+import background from "../assets/images/background1.jpg";
 
 const Register = () => {
   const [username, setUsername] = useState("");
@@ -57,98 +58,104 @@ const Register = () => {
     }
   };
   return (
-    <Row className="justify-content-center align-items-center vh-100 vw-100">
-      <Card style={{ width: "25rem" }}>
-        <Card.Body>
-          <Card.Title as="h2" className="text-center">
-            Register
-          </Card.Title>
-          <hr />
-          <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicName">
-              <Form.Label>UserName</Form.Label>
-              <Form.Control
-                className={`${
-                  username && !usernameIsValid ? "invalid is-invalid" : ""
-                } no__feedback shadow-none`}
-                value={username}
-                onChange={(e) => {
-                  setUsername(e.target.value);
-                }}
-                type="text"
-                placeholder="Enter username"
-              />
-              {usernameIsValid && username && (
-                <Form.Text style={{ color: "green" }}>
-                  Username is available!
-                </Form.Text>
-              )}
-              {usernameIsValid === false && username && (
-                <Form.Text style={{ color: "red" }}>
-                  Whoops! username already exists.
-                </Form.Text>
-              )}
-            </Form.Group>
+    <>
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="background"
+      ></div>
+      <Row className="justify-content-center align-items-center vh-100 vw-100">
+        <Card className="form" style={{ width: "25rem" }}>
+          <Card.Body>
+            <Card.Title as="h2" className="text-center">
+              Register
+            </Card.Title>
+            <hr />
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="formBasicName">
+                <Form.Label>UserName</Form.Label>
+                <Form.Control
+                  className={`${
+                    username && !usernameIsValid ? "invalid is-invalid" : ""
+                  } no__feedback shadow-none`}
+                  value={username}
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
+                  type="text"
+                  placeholder="Enter username"
+                />
+                {usernameIsValid && username && (
+                  <Form.Text style={{ color: "green" }}>
+                    Username is available!
+                  </Form.Text>
+                )}
+                {usernameIsValid === false && username && (
+                  <Form.Text style={{ color: "red" }}>
+                    Whoops! username already exists.
+                  </Form.Text>
+                )}
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="customEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                required
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                type="email"
-                placeholder="Enter email"
-              />
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="customEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  required
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  type="email"
+                  placeholder="Enter email"
+                />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword1">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-                type="password"
-                placeholder="Password"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword2">
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control
-                required
-                value={confirmPassword}
-                className={`${
-                  confirmPassword && !passIsValid ? "invalid is-invalid" : ""
-                } no__feedback shadow-none`}
-                onChange={(e) => {
-                  setPassIsValid(e.target.value === password);
-                  setConfirmPassword(e.target.value);
-                }}
-                type="password"
-                placeholder="Password"
-              />
-              {!passIsValid && (
-                <Form.Text style={{ color: "red" }}>
-                  passwords do not match
-                </Form.Text>
-              )}
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword1">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword2">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  required
+                  value={confirmPassword}
+                  className={`${
+                    confirmPassword && !passIsValid ? "invalid is-invalid" : ""
+                  } no__feedback shadow-none`}
+                  onChange={(e) => {
+                    setPassIsValid(e.target.value === password);
+                    setConfirmPassword(e.target.value);
+                  }}
+                  type="password"
+                  placeholder="Password"
+                />
+                {!passIsValid && (
+                  <Form.Text style={{ color: "red" }}>
+                    passwords do not match
+                  </Form.Text>
+                )}
+              </Form.Group>
 
-            <div className="d-grid gap-3">
-              <Button size="lg" variant="primary" type="submit">
-                Register
-              </Button>
-              <Card.Subtitle className="mb-2 text-muted">
-                Already have an account?{" "}
-                <Card.Link href="/login">login</Card.Link>
-              </Card.Subtitle>
-            </div>
-          </Form>
-        </Card.Body>
-      </Card>
-    </Row>
+              <div className="d-grid gap-3">
+                <Button size="lg" variant="primary" type="submit">
+                  Register
+                </Button>
+                <Card.Subtitle className="mb-2 text-muted">
+                  Already have an account?{" "}
+                  <Card.Link href="/login">login</Card.Link>
+                </Card.Subtitle>
+              </div>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Row>
+    </>
   );
 };
 
